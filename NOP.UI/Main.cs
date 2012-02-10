3a7222d827ef1de8162a9355ce7cb574c1590434
Tommi Johtela <tommi.johtela@dnainternet.net>
2012-02-10 21:39:42 +0200

    Added comment to Main.

--- NOP.UI/Main.cs
+++ NOP.UI/Main.cs
@@ -5,6 +5,12 @@ namespace NOP.UI
 {
 	class MainClass
 	{
+		/// <summary>
+		/// The entry point of the program, where the program control starts and ends.
+		/// </summary>
+		/// <param name='args'>
+		/// The command-line arguments.
+		/// </param>
 		public static void Main (string[] args)
 		{
 			Application.Init ();
