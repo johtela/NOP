67cd77a37bc4fcb2191d8bda84bd586ecb00a3b7
Tommi Johtela <tommi.johtela@dnainternet.net>
2012-02-10 21:44:34 +0200

    Changed the comments on Main.

--- NOP.UI/Main.cs
+++ NOP.UI/Main.cs
@@ -8,9 +8,6 @@ namespace NOP.UI
 		/// <summary>
 		/// The entry point of the program, where the program control starts and ends.
 		/// </summary>
-		/// <param name='args'>
-		/// The command-line arguments.
-		/// </param>
 		public static void Main (string[] args)
 		{
 			Application.Init ();
