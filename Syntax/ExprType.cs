c01160810cf98700daf8668c7750eb1e665a1d75
Tommi Johtela <tommi.johtela@dnainternet.net>
2012-05-15 22:21:20 +0300

    Added ExprType class for the type checking code.

---
+++ Syntax/ExprType.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HindleyMilner
+{
+    public abstract class ExprType
+    {
+        public class Lam : ExprType
+        {
+            public readonly ExprType Input, Result;
+
+            public Lam(ExprType input, ExprType result)
+            {
+                Input = input;
+                Result = result;
+            }
+        }
+
+        public class Var : ExprType
+        {
+            public readonly string Name;
+
+            public Var(string name)
+            {
+                Name = name;
+            }
+        }
+
+        public class Con : ExprType
+        {
+            public readonly string Name;
+            public readonly IEnumerable<ExprType> TypeArgs;
+
+            public Con(string name, IEnumerable<ExprType> typeArgs)
+            {
+                Name = name;
+                TypeArgs = typeArgs;
+            }
+        }
+    }
+}
