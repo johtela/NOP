6cf74b038ea83d593db655209ecddd29edc3a2ec
Tommi Johtela <tommi.johtela@dnainternet.net>
2012-07-12 21:06:59 +0300

    Completed previous commit.

--- Types/MonoType.cs
+++ Types/MonoType.cs
@@ -4,6 +4,19 @@
 	using System.Linq;
 	using NOP.Collections;
 
+	public class UnificationError : Exception
+	{
+		public readonly MonoType Type1;
+		public readonly MonoType Type2;
+			
+		public UnificationError (MonoType type1, MonoType type2) 
+				: base (string.Format ("Could not unify type {0} with {1}", type1, type2))
+		{
+			Type1 = type1;
+			Type2 = type2;
+		}
+	}
+
 	public abstract class MonoType
     {
 		private static int _lastVar;
