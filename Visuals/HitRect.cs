f24c65482098db85e5bf387e6c5062aed58b4440
Tommi Johtela <tommi.johtela@dnainternet.net>
2013-02-03 13:30:55 +0200

    Started to work with mouse selection.

---
+++ Visuals/HitRect.cs
@@ -0,0 +1,21 @@
+﻿namespace NOP.Visuals
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Drawing;
+	using NOP.Collections;
+
+	public class HitRect
+	{
+		public readonly RectangleF Rect;
+		public readonly SExpr SExp;
+
+		public HitRect (RectangleF rect, SExpr sexp)
+		{
+			Rect = rect;
+			SExp = sexp;
+		}
+	}
+}
