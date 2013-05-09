d831c3cfbfce3577d03fae476832bc81d5ba8e89
Tommi Johtela <tommi.johtela@dnainternet.net>
2013-05-09 23:14:57 +0300

    Moved code a bit to get rid of unnecessary classes.

--- IO/SExprTextStore.cs
+++ IO/SExprTextStore.cs
@@ -1,10 +1,42 @@
 ﻿namespace NOP.IO
 {
+	using System;
 	using System.IO;
 	using Parsing;
 
 	public class SExprTextStore : SExprStore<TextReader, TextWriter>
 	{
+		private static Parser<SExpr, char> sexpr;
+
+		static SExprTextStore ()
+		{
+			var type = StringParser.NoneOf (':').ManyChars ().Bind (typename =>
+						{
+							var t = Type.GetType (typename);
+							return t != null ?
+								t.ToParser<Type, char> () :
+								Parser.Fail<Type, char> (typename, "valid type name");
+						});
+
+			var list = from op in StringParser.Char ('(')
+					   from sexps in sexpr.Many ()
+					   from cp in StringParser.Char (')').SkipJunk ()
+					   select SExpr.Lst (sexps);
+
+			var symbol = from x in StringParser.Letter ()
+						 from xs in StringParser.AlphaNumeric ().Plus (StringParser.Char ('?')).Many ()
+						 select SExpr.Sym ((x | xs).ToString ());
+
+			var literal = from ob in StringParser.Char ('{')
+						  from t in type
+						  from col in StringParser.Char (':')
+						  from value in ConvertLiteral.GetParser (t)
+						  from cb in StringParser.Char ('}')
+						  select SExpr.Lit (value);
+
+			sexpr = list.Plus (symbol).Plus (literal).SkipJunk ();
+		}
+
 		public override void Write (SExpr sexp, TextWriter writer)
 		{
 			writer.Write (sexp.ToString ());
@@ -13,7 +45,7 @@
 		public override SExpr Read (TextReader reader)
 		{
 			var input = Input.FromString (reader.ReadToEnd ());
-			return SExprTextFormat.Parse (input);
+			return sexpr.Parse (input);
 		}
 	}
 }
